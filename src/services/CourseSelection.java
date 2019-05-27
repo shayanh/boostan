6bcd9dc76320f2829b7ec1c48a04261f8c5edcfe
Shayan Hosseini <shayan.hosseiny@gmail.com>
2019-05-27 14:50:27 +0430

    CourseSelection boilerplate

---
+++ src/services/CourseSelection.java
@@ -0,0 +1,16 @@
+package services;
+
+import models.CourseOffering;
+import models.Student;
+
+import java.util.ArrayList;
+
+public class CourseSelection {
+    public ArrayList<CourseOffering> getOfferings(Student student) {
+        return null;
+    }
+
+    public boolean selectOfferings(Student student, ArrayList<CourseOffering> offerings) {
+        return false;
+    }
+}
